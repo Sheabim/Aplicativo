330f57dc5afe41732bdab842b97880e77c489afb
shailabrazil <sheilaadesina85@gmail.com>
2018-10-05 10:41:48 +1000

    playing

--- js/app.js
+++ js/app.js
@@ -11,3 +11,21 @@ const myList = document.getElementsByTagName('li');
 for (let i = 0; i < myList.length; i += 1) {
    myList[i].style.color = 'purple';
 }
+
+
+const error = document.getElementsByClassName('error');
+
+for (let i = 0; i < error.length; i += 1) {
+   error[i].style.color = 'red';
+}
+
+
+
+const input = document.querySelector('input');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+p.textContent = input.value + ':';
+
+												});
